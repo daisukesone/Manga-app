2246b2a2cf5d3012cd93b7dc37dbf2d4e670c349
daisukesone <sonedai0213@gmail.com>
2020-12-04 23:21:20 +0900

    add admin page and devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_103403) do
+ActiveRecord::Schema.define(version: 2020_12_02_141104) do
+
+  create_table "mangas", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
